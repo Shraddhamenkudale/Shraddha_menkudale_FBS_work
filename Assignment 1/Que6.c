82d36d9e4a211925480700d04b0f9cfe08d31702
Shraddhamenkudale <menkudaleshraddha0912@gmail.com>
2026-07-27 21:44:04 +0530

    Add files via upload

---
+++ Assignment 1/Que6.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+	char ch;
+	printf("Enter a character");
+	scanf("%c",&ch);
+	
+	if(ch>='A' && ch<='Z' || ch>='a' && ch<='z')
+	{
+		printf("upper case");
+	}
+	else
+	{
+	    printf(" lower case");	
+	}
+}
---
+++ Assignment 1/Que7.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+	double bs;
+	printf("Enter basic salary");
+	scanf("%lf",&bs);
+	
+	double ts, da,hra, ta;
+	
+	if(bs<=5000)
+	{
+		da=bs*0.10;
+		ta=bs*0.20;
+		hra=bs*0.25;
+	}
+	else
+	{
+		da=bs*0.15;
+		ta=bs*0.25;
+		hra=bs*0.30;
+	}
+	
+	ts= bs+da+ta+hra;
+	printf("Total salary is %lf",ts);
+}
