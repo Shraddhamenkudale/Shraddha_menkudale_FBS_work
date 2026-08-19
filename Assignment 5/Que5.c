16b89221d8be142ae3ca3203a6975b2f9e9dffbc
Shraddhamenkudale <menkudaleshraddha0912@gmail.com>
2026-08-19 09:42:43 +0530

    Add files via upload

---
+++ Assignment 5/Que5.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main()
+{
+    int n = 5;
+    int i, j, space;
+
+    for(i = n; i >= 1; i--)
+    {
+    	
+        for(space = 1; space <= n - i; space++)
+        {
+            printf(" ");
+        }
+
+        for(j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
